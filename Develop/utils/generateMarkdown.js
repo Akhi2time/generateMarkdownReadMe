09b5c6a69cedd22fc639f552f7534a7685f344d0
Mohamed Mohamed <mohamedmohamed@infra04-wg368.wireless.umn.edu>
2022-09-06 08:07:51 -0500

    Second Commit

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,14 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) { 
+  console.log(license)
+  return `![Github License](https://img.shields.io/badge/license-${license}-blue.svg)`
+ }
+
+ function renderLicenseLink(){
+  return `[License Overview](https://gist.github.com/nicolasdao/a7adda51f2f185e8d2700e1573d8a633)`
+ }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
